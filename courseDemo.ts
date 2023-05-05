1cc549d115f4736126900aa7d714c03889f07eee
Liyw-liyw <liyuanwu_liyw@163.com>
2023-05-05 14:06:06 +0800

    feat: update text

--- courseDemo.ts
+++ courseDemo.ts
@@ -212,8 +212,7 @@ contract Whitelist {
     }
 }`;
 
-const elementaryLearningDemoSolidityCode = `
-// SPDX-License-Identifier: MIT
+const elementaryLearningDemoSolidityCode = `// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
