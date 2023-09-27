0adbcc43e23a71bf980763e066ca014ae9c8b4a3
hexi1997 <18715156450@163.com>
2023-09-27 13:50:20 +0800

    feat: add 404 page i18n

---
+++ locales/en/404.ts
@@ -0,0 +1,4 @@
+export default {
+  '404.title': `We can't find that page.`,
+  '404.button.text': 'Go Back Home'
+};
---
+++ locales/zh/404.ts
@@ -0,0 +1,4 @@
+export default {
+  '404.title': `找不到当前页面。`,
+  '404.button.text': '返回首页'
+};
