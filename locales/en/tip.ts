383f28adb19cb71b19c133039ab75d1f876ec8a2
hexi1997 <18715156450@163.com>
2023-10-18 14:27:28 +0800

    feat: update i18n

--- locales/en/tip.ts
+++ locales/en/tip.ts
@@ -1,5 +1,6 @@
 export default {
   'tip.success.login': 'Sign-in successful!',
+  'tip.login_again': 'Token expired, Please Sign-in again',
 
   'tip.error.start_learning': 'Error while start learning',
   'tip.error.complete_lesson': 'Error while complete lesson',
--- locales/zh/tip.ts
+++ locales/zh/tip.ts
@@ -1,5 +1,6 @@
 export default {
   'tip.success.login': '登录成功！',
+  'tip.login_again': 'Token已过期，请重新登录',
 
   'tip.error.start_learning': '开始学习时出错',
   'tip.error.complete_lesson': '完成课程时出错',
